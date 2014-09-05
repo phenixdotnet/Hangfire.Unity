629e4c1053bdd51442c616b012cb4dd6745778a2
Vincent Lainé <phenixdotnet@gmail.com>
2014-09-05 10:25:45 +0200

    Added initial implementation of unity container for job activator

---
+++ src/Hangfire.Unity/UnityBootstrapperConfigurationExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Hangfire.Unity;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangfire
+{
+    /// <summary>
+    /// Extensions class for unity
+    /// </summary>
+    public static class UnityBootstrapperConfigurationExtensions
+    {
+        /// <summary>
+        /// Tells bootstrapper to use the specified Ninject
+        /// kernel as a global job activator.
+        /// </summary>
+        /// <param name="configuration">Configuration</param>
+        /// <param name="container">The unity container that will be used to activate jobs</param>
+        public static void UseUnityActivator(this IBootstrapperConfiguration configuration, UnityContainer container)
+        {
+            configuration.UseActivator(new UnityJobActivator(container));
+        }
+    }
+}
---
+++ src/Hangfire.Unity/UnityJobActivator.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangfire.Unity
+{
+    /// <summary>
+    /// A job activator which use unity to create job instance
+    /// </summary>
+    public class UnityJobActivator : JobActivator
+    {
+        private readonly UnityContainer container;
+
+        /// <summary>
+        /// Initialize a new instance of the <see cref="T:UnityJobActivator"/> class
+        /// </summary>
+        /// <param name="container">The unity container to be used</param>
+        public UnityJobActivator(UnityContainer container)
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+
+            this.container = container;
+        }
+
+        /// <inheritdoc />
+        public override object ActivateJob(Type jobType)
+        {
+            
+            return this.container.Resolve(jobType);
+        }
+    }
+}
